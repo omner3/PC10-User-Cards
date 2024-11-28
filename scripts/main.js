0cfb61ab0f117e4deaf6cd6fa5a05488f248282d
@Omner3 <gonzalezomner@hotmail.com>
2024-11-27 19:59:49 -0600

    verificación del commit anterior guardado

--- scripts/main.js
+++ scripts/main.js
@@ -7,13 +7,17 @@ function fetchUsers() {
     function loadUsers(data) {
         usersList.innerHTML = ""
         data.forEach(element => {
-            let users_template = `            
-                <h5 class="card-title">Nombre: ${element.name}</h5>    
+            let users_template = `
+            <div class="card card text-bg-info mb-3" style="width: 18rem;">
+                <div class="card-body">            
+                    <h5 class="card-title">Nombre: ${element.name}</h5>    
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Nombre de usuario: ${element.username}</li>
+                    <li class="list-group-item">Correo electrónico: ${element.email}</li>
+                    <li class="list-group-item">Empresa: A ${element.company.name}</li>
+                </ul>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Nombre de usuario: ${element.username}</li>
-                <li class="list-group-item">Correo electrónico: ${element.email}</li>
-                <li class="list-group-item">Empresa: A ${element.company.name}</li>
             `
             usersList.innerHTML += users_template
         });
